f88814ff22677c422e1883d89d6370705d6c37ce
Brian-Mas <brianmasschaele@icloud.com>
2016-12-07 17:53:13 +0100

    Added Facebook SDK
    Added Facebook Login
    Get token successfull
    Get Email successfull
    
    Next Step: Register on Tapio Back-end

---
+++ Tests/FacebookLoginTest/app/src/main/java/be/howest/nmct/facebooklogintest/MainActivity.java
@@ -0,0 +1,87 @@
+package be.howest.nmct.facebooklogintest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView info, emailTextview;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.activity_main);
+        info = (TextView) findViewById(R.id.info);
+        emailTextview = (TextView) findViewById(R.id.email);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                String authToken = loginResult.getAccessToken().getToken();
+                info.setText("user id: " + loginResult.getAccessToken().getUserId() +
+                        "\n AuthToken: " + authToken);
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.v("LoginActivity", response.toString());
+
+                                // Application code
+                                try {
+                                    String email = object.getString("email");
+//                                    String birthday = object.getString("birthday"); // 01/31/1980 format
+                                    emailTextview.setText(email);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                info.setText("Login failed");
+            }
+        });
+
+
+    }
+}
